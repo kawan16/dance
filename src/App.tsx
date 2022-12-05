8de085787244760bc23e0244b972d76d8447f2ad
kawan16 <karl.devooght@gmail.com>
2022-12-05 22:44:02 +0100

    -

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,64 @@ import React, {useState} from 'react';
 import DanceMoves from './bachata.json';
 import './App.css'
 
+const MoveTypes = DanceMoves
+    .reduce((types: any, move: any) => {
+        const newTypes = {...types};
+        console.log(move.name, ' ', move.type)
+        newTypes[move.type] = newTypes[move.type] || [];
+        return newTypes;
+    }, {});
+
 function App() {
-    const [filter, setFilter] = useState('');
+    const [filter, setFilter] = useState({name: '', type: ''});
+    const setNameFilter = (name: any) => setFilter({...filter, name});
+    const setTypeFilter = (type: any) => setFilter({...filter, type});
     return (
         <div className="container">
-            <MoveSearchBar
-                onFilter={setFilter}/>
-            <MoveList filter={filter}/>
+            <div className="columns">
+                <div className="column is-one-fifth">
+                    <MoveTypeFilterMenu
+                        selectedType={filter.type}
+                        onSelectedType={setTypeFilter}/>
+                </div>
+                <div className="column">
+                    <MoveSearchBar
+                        onFilter={setNameFilter}/>
+                    <MoveList filter={filter}/>
+                </div>
+            </div>
         </div>
     );
+}
+
+function MoveTypeFilterMenu(props: any) {
+    const types = Object.keys(MoveTypes).sort();
+    return (
+        <aside className="menu">
+            <p className="menu-label">
+                Type
+            </p>
+            <ul className="menu-list">
+                <li>
+                    <a
+                        className={props.selectedType === '' ? 'is-active' : ''}
+                        onClick={() => props.onSelectedType('')}>
+                        {'All (' + DanceMoves.length + ')'}
+                    </a>
+                </li>
+                {
+                    types.map((moveType:any) =>
+                        <li key={moveType}>
+                            <a className={moveType === props.selectedType ? 'is-active' : ''}
+                                onClick={() => props.onSelectedType(moveType)}>
+                                {moveType}
+                            </a>
+                        </li>
+                    )
+                }
+            </ul>
+        </aside>
+    )
 }
 
 function MoveSearchBar(props: any) {
@@ -28,7 +77,9 @@ function MoveSearchBar(props: any) {
 }
 
 function MoveList(props: any) {
-    const matchMove = (danceMove: any) => danceMove.move.toLowerCase().includes(props.filter.toLowerCase());
+    const matchMove = (danceMove: any) =>
+        danceMove.name.toLowerCase().includes(props.filter.name.toLowerCase()) &&
+        danceMove.type?.toLowerCase().includes(props.filter.type.toLowerCase());
     const filteredMoves = DanceMoves.filter(matchMove);
     return (
         <div className="columns is-multiline">
@@ -41,7 +92,9 @@ function MoveList(props: any) {
 
 function MoveListItem(props: any) {
     return (
-        <div className="column is-one-quarter">
+        <div
+            key={props.name}
+            className="column is-one-quarter">
             <MoveCard {...props}/>
         </div>
     )
@@ -58,13 +111,16 @@ function MoveCard(props: any) {
             <div className="card-content">
                 <div className="media">
                     <div className="media-content">
-                        <p className="title is-4">{props.move}</p>
+                        <p className="title is-4">{props.name}</p>
                         <p className="subtitle is-6">{props.dance}</p>
                     </div>
                 </div>
             </div>
             <footer className="card-footer">
-                <a href={props.videoUrl} className="card-footer-item">
+                <a
+                    href={props.videoUrl}
+                    target="_blank"
+                    className="card-footer-item">
                     Watch
                 </a>
             </footer>
